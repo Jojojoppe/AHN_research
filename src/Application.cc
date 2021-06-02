3097d7e26928992273a32ac4a4a7d278ea139369
Joppe Blondel <joppe@blondel.nl>
2021-06-02 13:46:04 +0200

    Start with RTS/CTS messages
    Fixed timing issues
    
    Signed-off-by: Joppe Blondel <joppe@blondel.nl>

--- src/Application.cc
+++ src/Application.cc
@@ -18,6 +18,7 @@
 #include <memory>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 using namespace inet;
 using namespace omnetpp;
@@ -30,6 +31,9 @@ Application::Application(inet::ApplicationBase * parent, std::function<void(std:
 
     // get simulation parameters
     beaconPeriod = parent->getParentModule()->getAncestorPar("beaconPeriod");
+    dataExchangeInterval = parent->getParentModule()->getAncestorPar("dataExchangeInterval");
+    nrPackets = parent->getParentModule()->getAncestorPar("nrPackets");
+    nrBytesPerPacket = parent->getParentModule()->getAncestorPar("nrBytesPerPacket");
     
     this->ID = parent->getId();
 }
@@ -38,12 +42,23 @@ Application::~Application(){
 }
 
 void Application::startApplication(){
+    // Beacon generation
+    // -----------------
     // Get random value between 0 and 1;
     auto randwait_i = parent->intrand(1000);
     float randwait = randwait_i/1000.0;
-
+    // Start beacon sending
     std::function<void()> callback = std::bind(&Application::beaconCallback, this);
-    timerManager->create(veins::TimerSpecification(callback).oneshotIn(simTime() + randwait*beaconPeriod));
+    timerManager->create(veins::TimerSpecification(callback).oneshotIn(randwait*beaconPeriod));
+    // -----------------
+
+    // Schedule mmw MAC main loop
+    callback = std::bind(&Application::mmw_loop, this);
+    timerManager->create(veins::TimerSpecification(callback).oneshotIn(parent->getParentModule()->getAncestorPar("mmwLoopTime")));
+
+    std::ostringstream appname;
+    appname << ID;
+    parent->getParentModule()->setName(appname.str().c_str());
 }
 
 void Application::stopApplication(){
@@ -56,48 +71,155 @@ void Application::processPacket(std::shared_ptr<inet::Packet> pk){
     std::istringstream beaconContent(content);
     std::string tmp;
 
+    // Get beacon values
+    // -----------------
     beaconContent >> tmp; beaconContent >> tmp;
     int m_id = std::stoi(tmp);
     beaconContent >> tmp; beaconContent >> tmp;
     int m_x = std::stoi(tmp);
     beaconContent >> tmp; beaconContent >> tmp;
     int m_y = std::stoi(tmp);
+    beaconContent >> tmp; beaconContent >> tmp;
+    std::string m_type = tmp;
+
+    std::string m_rts_rx;
+    double m_rts_dur;
+    std::string m_cts_rx;
+    std::string m_cts_del;
+
+    if(m_type=="RTS"){
+        beaconContent >> tmp; beaconContent >> tmp;
+        m_rts_rx = tmp;
+        beaconContent >> tmp; beaconContent >> tmp;
+        m_rts_dur = std::stod(tmp);
+    }else if(m_type=="CTS"){
+        beaconContent >> tmp; beaconContent >> tmp;
+        m_cts_rx = tmp;
+        beaconContent >> tmp; beaconContent >> tmp;
+        m_cts_del = tmp;
+    }
+    // -----------------
 
     // Check if self message
     if(m_id == this->ID) return;
 
+    // Print beacon content
+    // --------------------
+    EV_INFO << content << std::endl;
     EV_INFO << "m_id : " << m_id << std::endl;
     EV_INFO << "m_x : " << m_x << std::endl;
     EV_INFO << "m_y : " << m_y << std::endl;
+    EV_INFO << "m_type: " << m_type << std::endl;
 
-    // Get position from mobility
-    cModule * ppmodule = parent->getParentModule()->getSubmodule("mobility");
-    IMobility *mmobility = check_and_cast<IMobility *>(ppmodule);
-    Coord pos = mmobility->getCurrentPosition();
-    // Draw line between self and txer
-    auto * line = createLine(pos.x, pos.y, m_x, m_y, "purple", 4);
-    showLine(line);
+    if(m_type=="RTS"){
+        EV_INFO << "m_rts_rx: " << m_rts_rx << std::endl;
+        EV_INFO << "m_rts_dur: " << m_rts_dur << std::endl;
+    }else if(m_type=="CTS"){
+        EV_INFO << "m_cts_rx: " << m_cts_rx << std::endl;
+        EV_INFO << "m_cts_del: " << m_cts_del << std::endl;
+    }
+    // --------------------
+
+    // Update neighbor list
+    if(std::find(neighbors.begin(), neighbors.end(), m_id)==neighbors.end()){
+        // Not yet in neighbor list, add to it
+        neighbors.push_back(m_id);
+        EV_INFO << "New neighbor: [";
+        for(auto & ii : neighbors) EV_INFO << ii << ", ";
+        EV_INFO << "]" << std::endl;
+    }
+
+    if(m_type=="RTS"){
+        std::stringstream ss(m_rts_rx);
+        std::vector<int> rxers;
+        while(ss.good()){
+            std::string substr;
+            getline(ss, substr, ',');
+            if(substr!=""){
+                rxers.push_back(std::stoi(substr));
+            }
+        }
+        mmw_recv_rts(m_id, m_rts_dur, rxers);
+    }else if(m_type=="CTS"){
+        std::stringstream ssrx(m_cts_rx);
+        std::vector<int> rxers;
+        while(ssrx.good()){
+            std::string substr;
+            getline(ssrx, substr, ',');
+            if(substr!=""){
+                rxers.push_back(std::stoi(substr));
+            }
+        }
+        std::stringstream ssdel(m_cts_del);
+        std::vector<double> delays;
+        while(ssdel.good()){
+            std::string substr;
+            getline(ssdel, substr, ',');
+            if(substr!=""){
+                delays.push_back(std::stod(substr));
+            }
+        }
+        mmw_recv_cts(m_id, delays, rxers);
+    }
+
 }
 
 void Application::beaconCallback(){
     std::function<void()> callback = std::bind(&Application::beaconCallback, this);
-    timerManager->create(veins::TimerSpecification(callback).oneshotIn(simTime() + beaconPeriod));
+    timerManager->create(veins::TimerSpecification(callback).oneshotIn(beaconPeriod));
 
     auto payload = makeShared<VeinsInetSampleMessage>();
     payload->removeTagIfPresent<CreationTimeTag>(b(0), b(-1));
     auto creationTimeTag = payload->addTag<CreationTimeTag>();
     creationTimeTag->setCreationTime(simTime());
-    payload->setChunkLength(B(500));
+    payload->setChunkLength(B(800));
 
     // Set beacon content
+    // ------------------
     std::ostringstream beaconContent;
+    // Add ID
     beaconContent << "ID " << this->ID << " ";
-    // Get position from mobility
-    cModule * ppmodule = parent->getParentModule()->getSubmodule("mobility");
-    IMobility *mmobility = check_and_cast<IMobility *>(ppmodule);
-    Coord pos = mmobility->getCurrentPosition();
+    // Add position
+    Coord pos = getPos();
     beaconContent << "x " << pos.x << " ";
     beaconContent << "y " << pos.y << " ";
+
+    // Check if there is RTS to transmit
+    if(sendRts){
+        mmw_send_rts();
+        beaconContent << "tp RTS ";
+        beaconContent << "rx ";
+        for(auto & ii : rts_rxNodes){
+            beaconContent << ii << ",";
+        }
+        beaconContent << " ";
+        beaconContent << "dur " << rts_transmissionDuration << " ";
+
+        sendRts = false;
+        rts_rxNodes.clear();
+    }
+    // Check if there is CTS to transmit
+    else if(sendCts){
+        mmw_send_cts();
+        beaconContent << "tp CTS ";
+        beaconContent << "rx ";
+        for(auto & ii : cts_rxNodes){
+            beaconContent << ii << ",";
+        }
+        beaconContent << " del ";
+        for(auto & ii : cts_delays){
+            beaconContent << ii << ",";
+        }
+        beaconContent << " ";
+
+        sendCts = false;
+    }
+    // If no special beacon
+    else{
+        beaconContent << "tp NONE ";
+    }
+
+    // ------------------
     // Set content
     payload->setRoadId(beaconContent.str().c_str());
 
@@ -120,6 +242,12 @@ Application * Application::getAppFromId(int id){
     }
 }
 
+Coord Application::getPos(){
+    cModule * ppmodule = parent->getParentModule()->getSubmodule("mobility");
+    IMobility *mmobility = check_and_cast<IMobility *>(ppmodule);
+    return mmobility->getCurrentPosition();
+}
+
 cPolylineFigure * Application::createLine(double x1, double y1, double x2, double y2, const char * color, double width){
     std::vector<cFigure::Point> points = {cFigure::Point(x1, y1), cFigure::Point(x2, y2)};
     auto * line = new cPolylineFigure();
@@ -137,4 +265,125 @@ void Application::showLine(cPolylineFigure * line){
 void Application::deleteLine(cPolylineFigure * line){
     cCanvas * canvas = parent->getSimulation()->getSystemModule()->getCanvas();
     canvas->removeFigure(line);
+}
+
+void Application::showLineTimed(double x1, double y1, double x2, double y2, const char * color, double width, double time){
+    auto * line = createLine(x1, y1, x2, y2, color, width);
+    showLine(line);
+
+    std::function<void()> callback = std::bind(&Application::deleteLine, this, line);
+    timerManager->create(veins::TimerSpecification(callback).oneshotIn(time));
+}
+
+bool Application::startTransmissionRx(int packets, int bytesPerPacket, double time, Coord txPos){
+
+    return true;
+}
+
+void Application::startTransmissionTx(){
+    // Get first transmission from list and delete it
+    int rxer = -1;
+    double time = +1.0/0.0;
+    int i = 0;
+    int rxer_i = 0;
+    for(auto & ii : mmw_outgoing_scheduled){
+        if(ii.second<time){
+            time = ii.second;
+            rxer = ii.first;
+            rxer_i = i;
+        }
+        i++;
+    }
+    mmw_outgoing_scheduled.erase(mmw_outgoing_scheduled.begin() + rxer_i);
+
+    EV_INFO << "Transmit mmWave data to " << rxer << std::endl;
+
+    auto * app = getAppFromId(rxer);
+    bool successful = app->startTransmissionRx(nrPackets, nrBytesPerPacket, dataExchangeInterval, getPos());
+    if(successful){
+        Coord txPos = getPos();
+        Coord rxPos = app->getPos();
+        showLineTimed(rxPos.x, rxPos.y, txPos.x, txPos.y, "purple", 4, dataExchangeInterval);
+    }
+}
+
+// --------------------
+// mmWave MAC functions
+// --------------------
+
+// This function is ran each x amount of time
+void Application::mmw_loop(){
+    nodeName = parent->getParentModule()->getName();
+    std::function<void()> callback = std::bind(&Application::mmw_loop, this);
+    timerManager->create(veins::TimerSpecification(callback).oneshotIn(parent->getParentModule()->getAncestorPar("mmwLoopTime")));
+
+    // DEBUG
+    // Let node0 start transmission once when 4 neighbors are there
+    if(nodeName=="52" && neighbors.size()>=4 && db_oneshotTransmission){
+        db_oneshotTransmission = false;
+        EV_INFO << "send RTS" << std::endl;
+
+        // Send RTS
+        sendRts = true;
+    }
+}
+
+// This function is ran when an RTS is received
+void Application::mmw_recv_rts(int txer, double duration, std::vector<int> & rxers){
+    // Check if I'm in RTS message
+    if(std::find(rxers.begin(), rxers.end(), ID)!=rxers.end()){
+        // NEED TO RECEIVE DATA
+        sendCts = true;
+        mmw_todo[txer] = duration;
+    }
+
+    // Set ongoing transmissions list (reset if transmitter starts again with RTS)
+    mmw_ogt_dur[txer] = duration;
+    mmw_ogt_rxers[txer] = std::vector<int>(rxers);
+    mmw_ogt_startime[txer] = simTime().dbl();
+}
+
+// This function is ran when a CTS is received
+void Application::mmw_recv_cts(int txer, std::vector<double> & delays, std::vector<int> & rxers){
+    // Check if CTS is for me
+    if(std::find(rxers.begin(), rxers.end(), ID)!=rxers.end()){
+        // NEED TO TRANSMIT DATA
+        for(int i=0; i<delays.size(); i++){
+            double del = delays[i];
+            del += 0.0001;  // Add to allow for scheduling
+
+            EV_INFO << "CTS received from " << txer << " with delay " << del << std::endl;
+            EV_INFO << "Schedule transmission in " << simTime() + del << " (now it is " << simTime() << " )" << std::endl;
+
+            // Schedule transmission
+            mmw_outgoing_scheduled.push_back(std::make_pair(txer, simTime().dbl() + del));
+            std::function<void()> callback = std::bind(&Application::startTransmissionTx, this);
+            timerManager->create(veins::TimerSpecification(callback).oneshotIn(del));
+        }
+    }else{
+        // NEED TO USE DATA FOR SCHEDULING
+    }
+}
+
+// This function is called when its sending an RTS
+void Application::mmw_send_rts(){
+    rts_rxNodes = neighbors;
+    rts_transmissionDuration = 0.050;
+}
+
+// This function is called when its sending a CTS
+void Application::mmw_send_cts(){
+    cts_rxNodes.clear();
+    cts_delays.clear();
+
+    // Check which transmissions must be done
+    for(auto & tr : mmw_todo){
+        int rxer = tr.first;
+        double duration = tr.second;
+
+        cts_rxNodes.push_back(rxer);
+        cts_delays.push_back(0.0);
+    }
+
+    mmw_todo.clear();
 }
--- src/Application.h
+++ src/Application.h
@@ -7,12 +7,16 @@
 #include "inet/common/INETDefs.h"
 #include "inet/applications/base/ApplicationBase.h"
 #include "inet/transportlayer/contract/udp/UdpSocket.h"
+#include "inet/common/geometry/common/Coord.h"
 
 #include <omnetpp.h>
 using namespace omnetpp;
 
 #include <functional>
 #include <memory>
+#include <vector>
+#include <unordered_map>
+#include <utility>
 
 class Application{
 private:    
@@ -20,12 +24,45 @@ private:
     std::function<void(std::unique_ptr<inet::Packet>)> sendPacket;
     veins::TimerManager * timerManager;
 
+    std::string nodeName;
+
     // Simulation parameters
     double beaconPeriod;
+    double dataExchangeInterval;
+    int nrPackets;
+    int nrBytesPerPacket;
 
     // Important constants
     int ID;
     bool staticApplication;
+
+    // Vehicle state
+    std::vector<int> neighbors;
+
+    // TX state
+    bool sendRts = false;
+    std::vector<int> rts_rxNodes;
+    double rts_transmissionDuration;
+
+    // Rx state
+    bool sendCts = false;
+    std::vector<int> cts_rxNodes;
+    std::vector<double> cts_delays;
+
+    // Scheduling
+    // ----------
+    // Ongoing transmissions
+    std::unordered_map<int, std::vector<int>> mmw_ogt_rxers;
+    std::unordered_map<int, double> mmw_ogt_dur;
+    std::unordered_map<int, double> mmw_ogt_startime;
+    std::unordered_map<int, std::vector<std::pair<int, double>>> mmw_ogt_scheduled;
+    // My transmissions
+    std::unordered_map<int, double> mmw_todo;
+    std::vector<std::pair<int, double>> mmw_outgoing_scheduled;
+    // ----------
+
+    // DEBUG
+    bool db_oneshotTransmission = true;
 
 public:
     Application(inet::ApplicationBase * parent, std::function<void(std::unique_ptr<inet::Packet>)> sendPacket, veins::TimerManager * timerManager, bool staticApplication);
@@ -38,12 +75,26 @@ public:
 
     void beaconCallback();
 
+    // Returns false if no transmission can be done
+    bool startTransmissionRx(int packets, int bytesPerPacket, double time, inet::Coord txPos);
+
 private:
     Application * getAppFromId(int id);
+    inet::Coord getPos();
 
     cPolylineFigure * createLine(double x1, double y1, double x2, double y2, const char * color, double width);
     void showLine(cPolylineFigure * line);
     void deleteLine(cPolylineFigure * line);
+    void showLineTimed(double x1, double y1, double x2, double y2, const char * color, double width, double time);
+
+    void startTransmissionTx();
+
+    // mmWave MAC functions
+    void mmw_loop();
+    void mmw_recv_rts(int txer, double duration, std::vector<int> & rxers);
+    void mmw_recv_cts(int txer, std::vector<double> & delays, std::vector<int> & rxers);
+    void mmw_send_rts();
+    void mmw_send_cts();
 };
 
 #endif
